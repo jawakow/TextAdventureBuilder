cfb589fe34981e1e9510ee1dd1eb92bfaf44ca02
jawakow <jawakow@gmail.com>
2020-02-03 14:33:10 +0000

    added other classes fields

--- TextAdventureBuilder/Room.cs
+++ TextAdventureBuilder/Room.cs
@@ -7,11 +7,17 @@ namespace TextAdventureBuilder
 		public Room ()
 		{
 		}
-        string northRoomId;
-        string southRoomId;
-        string eastRoomId;
-        string westRoomId;
-
+        string Id;
+        string northDoorRoomId;
+        string southDoorRoomId;
+        string eastDoorRoomId;
+        string westDoorRoomId;
+        string isNorthAccessible;
+        string isSouthAccessible;
+        string isEastAccessible;
+        string isWestAccessible;
+        string[] contents;
+        bool isDark;
 	}
 }
 
