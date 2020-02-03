c341cfab20a9a86dff865dec5dabf10b41edaeb2
Lewis McCormac <lmccormac@controlsoft.ie>
2020-02-03 18:01:19 +0000

    integrated Gints' gui

---
+++ TextAdventureBuilderGui/GameWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TextAdventureBuilder
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NorthButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SouthButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EastButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InventoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TextAdventureBuilderGui/StartWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextAdventureBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+            this.Close();
+        }
+    }
+}
