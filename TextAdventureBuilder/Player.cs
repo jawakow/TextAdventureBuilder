d3b75581db87b71ec287cabde3bab59e19c60a76
Lukasz Kubisz <18682311+jawakow@users.noreply.github.com>
2020-02-04 10:04:39 +0000

    Update Player.cs
    added movement feature

--- TextAdventureBuilder/Player.cs
+++ TextAdventureBuilder/Player.cs
@@ -14,6 +14,38 @@ namespace TextAdventureBuilder
 		public Player ()
 		{
 		}
+		
+		// return 1 if movement successful, otherwise return 0			
+		public int Move(string direction, Room room)
+		{
+	
+			if (direction = "N" && room.isNorthAccessible)
+				{
+				this.roomId = room.northDoorRoomId;
+				return 1;
+			}
+
+			if (direction = "S" && room.isSouthAccessible)
+				{
+				this.roomId = room.southDoorRoomId;
+				return 1;
+			}
+
+			if (direction = "W" && room.isWestAccessible)
+				{
+				this.roomId = room.westDoorRoomId;
+				return 1;
+			}
+
+			if (direction = "E" && room.isEastAccessible)
+				{
+				this.roomId = room.EastDoorRoomId;
+				return 1;
+			}
+
+			return 0;
+
+		}
 	}
 }
 
