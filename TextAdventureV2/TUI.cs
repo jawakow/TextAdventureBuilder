3547dab4a5221113567e5ea23b5d7a03b176f4a2
jawakow <jawakow@gmail.com>
2020-02-04 13:09:26 +0000

    added Text User Interface class

---
+++ TextAdventureV2/TUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class TUI
+{
+	public string playerName;
+	
+	public TUI()
+	{
+	}
+
+	void Run()
+    {
+		Console.Clear();
+		playername = Console.ReadLine("Enter player name");
+    }
+}
