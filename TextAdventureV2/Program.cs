b3cab361c0cc0a6413e10f2f59947ed26eeaf4fb
jawakow <jawakow@gmail.com>
2020-02-10 13:31:40 +0000

    working on movement

--- TextAdventureV2/Program.cs
+++ TextAdventureV2/Program.cs
@@ -9,23 +9,26 @@ namespace TextAdventureV2
     {
         static void Main(string[] args)
         {
+            
+
             XMLParser xmlParser = new XMLParser();
 
             Adventure adventure = xmlParser.ParseXml("../../../../Docs/AdventureTestScript.xml");
             MainActivity mainActivity = new MainActivity(adventure);
+            TUI tui = new TUI();
+            tui.play();
             //mainActivity.CheckCompletion();
 
             //Console.WriteLine("Hello World!");
             //XmlTextReader textReader = new XmlTextReader("../../../../Docs/AdventureTestScript.xml");
             //textReader.Read();
 
-<<<<<<< HEAD
-            TUI tui = new TUI();
-            tui.play();
-=======
+
+
+
             //tui = new TUI();
             //tui.Run();
->>>>>>> 27c44d1df9a77eaf6a4320bfb91433d31b193f94
+
 
 
             ////while (textReader.Read())
